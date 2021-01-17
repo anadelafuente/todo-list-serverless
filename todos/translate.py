72dd1e70241ac420d3e8a1e3ce0cda9ca3455d90
anadelafuente <76956564+anadelafuente@users.noreply.github.com>
2021-01-17 15:31:49 +0100

    Update translate.py

--- todos/translate.py
+++ todos/translate.py
@@ -1,2 +1,10 @@
 #Código función lambda Translate
 #devuelve una entrada de la ToDo list, traducida al idioma que se solicite a través del API
+import json
+import time
+import logging
+import os
+
+from todos import decimalencoder
+import boto3
+dynamodb = boto3.resource('dynamodb')
