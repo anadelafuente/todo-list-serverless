d824de39959fe2b9690ba4cb344a71cc9167a96e
EC2 Default User <ec2-user@ip-172-31-45-95.ec2.internal>
2021-01-19 09:37:05 +0000

    Creating translate funtion

--- todos/translate.py
+++ todos/translate.py
@@ -1,10 +1,37 @@
 #Código función lambda Translate
 #devuelve una entrada de la ToDo list, traducida al idioma que se solicite a través del API
-import json
-import time
 import logging
-import os
-
-from todos import decimalencoder
+import json
 import boto3
-dynamodb = boto3.resource('dynamodb')
+import os
+import time
+from todos import decimalencoder
+translate = boto3.client('translate')
+dynamodb = boto3.client('dynamodb')
+TABLE_NAME = os.getenv('TABLE_NAME')
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+
+def traducir(event, context):
+    data = json.loads(event['body'])
+    source_language = "auto"#reconocimiento automático con Comprehend
+    table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
+
+    # fetch todo from the database
+    response = table.get_item(
+        Key={
+           'id': event['pathParameters']['id']
+        }
+    )
+    result = translate.translate_text(response['text'], source_language, data['language'])
+
+    # create a response
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(result.get('TranslatedText'),
+                           cls=decimalencoder.DecimalEncoder)
+    }
+
+    return response
